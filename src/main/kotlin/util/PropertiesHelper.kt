364a2a09573c51c05929a521a705dd887e4cd162
William Gates <william.gates.3299@gmail.com>
2024-01-08 19:25:00 +0330

    Created PropertiesHelper Class

---
+++ src/main/kotlin/util/PropertiesHelper.kt
@@ -0,0 +1,28 @@
+package util
+
+import java.io.File
+import java.io.FileInputStream
+import java.util.*
+
+object PropertiesHelper {
+
+    private val rootDir = System.getProperty("user.dir")
+
+    private fun getLocalProperties(): Properties {
+        return Properties().apply {
+            FileInputStream(File("$rootDir/local.properties")).use(::load)
+        }
+    }
+
+    fun getBotToken(): String {
+        return getLocalProperties()
+            .getProperty(Constants.PROPERTY_BOT_TOKEN)
+            ?: throw IllegalStateException("Property named \"${Constants.PROPERTY_BOT_TOKEN}\" was not found.")
+    }
+
+    fun getDbPath(): String {
+        return getLocalProperties()
+            .getProperty(Constants.PROPERTY_LOCAL_DB_PATH)
+            ?: throw IllegalStateException("Property named \"${Constants.PROPERTY_BOT_TOKEN}\" was not found.")
+    }
+}
