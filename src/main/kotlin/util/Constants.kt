cf752a03f8e460f5933eec57bf278c379322524f
William Gates <william.gates.3299@gmail.com>
2024-01-08 19:25:20 +0330

    Minor Improvements

---
+++ src/main/kotlin/util/Constants.kt
@@ -0,0 +1,39 @@
+package util
+
+object Constants {
+
+    internal const val DEFAULT_PLUS_CREDIT = 20L
+    internal const val DEFAULT_MINUS_CREDIT = -20L
+
+    internal const val DEFAULT_PLUS_RICE_PLATE_CREDIT = 40L
+
+    internal const val COMMAND_RATING = "rating"
+    internal const val COMMAND_MY_RATING = "my_rating"
+
+    internal const val PROPERTY_BOT_TOKEN = "PROPERTY_BOT_TOKEN"
+    internal const val PROPERTY_LOCAL_DB_PATH = "PROPERTY_LOCAL_DB_PATH"
+
+    internal val plusSocialCreditStickers = arrayOf(
+        "AgADAgADf3BGHA",
+        "AgADPA0AAtKZWEg"
+    )
+
+    internal val minusSocialCreditStickers = arrayOf(
+        "AgADAwADf3BGHA",
+        "AgADLBEAAtc2WEg"
+    )
+
+    internal val plusRicePlateStickers = arrayOf(
+        "AgADugwAAkaXMUo"
+    )
+
+    internal val uyghurCampJobs = arrayOf(
+        "делать кроссовки Adidas",
+        "шить носки Nike",
+        "шить трусай H&M",
+        "делать майка Uniqlo",
+        "собирать Mercedes-Benz CLA",
+        "смазывать сцеплений DSG для VAG",
+        "собирать iPad"
+    )
+}
