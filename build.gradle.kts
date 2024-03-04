13eaadbd393056923c8638383c1cefe034051106
William Gates <william.gates.3299@gmail.com>
2024-03-04 12:02:44 +0330

    Upgraded Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,12 +29,15 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // Exposed SQL library
-    implementation("org.jetbrains.exposed:exposed-core:0.45.0")
-    implementation("org.jetbrains.exposed:exposed-dao:0.45.0")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.45.0")
+    implementation("org.jetbrains.exposed:exposed-core:0.46.0")
+    implementation("org.jetbrains.exposed:exposed-dao:0.46.0")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.46.0")
 
     // SQLite JDBC Library
-    implementation("org.xerial:sqlite-jdbc:3.44.1.0")
+    implementation("org.xerial:sqlite-jdbc:3.45.0.0")
+}
+repositories {
+    mavenCentral()
 }
 
 tasks.withType<KotlinCompile> {
