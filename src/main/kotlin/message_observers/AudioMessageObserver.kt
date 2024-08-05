fd3bb7593e54a901c6d9955adc148ebdcfa3f470
William <william.gates.3299@gmail.com>
2024-08-06 01:40:15 +0330

    Minor Improvements

---
+++ src/main/kotlin/message_observers/AudioMessageObserver.kt
@@ -0,0 +1,41 @@
+package message_observers
+
+import dev.inmo.micro_utils.coroutines.subscribe
+import dev.inmo.tgbotapi.extensions.behaviour_builder.BehaviourContext
+import dev.inmo.tgbotapi.extensions.utils.shortcuts.audioMessages
+import dev.inmo.tgbotapi.types.chat.ForumChatImpl
+import dev.inmo.tgbotapi.types.chat.GroupChatImpl
+import dev.inmo.tgbotapi.types.chat.SupergroupChatImpl
+import dev.inmo.tgbotapi.utils.RiskFeature
+import kotlinx.coroutines.CoroutineScope
+import utils.MessageHelper.sendLongLiveTheKingSticker
+import utils.MessageHelper.sendWomenGif
+import utils.MessageHelper.shouldSendLongLiveTheKingSticker
+import utils.MessageHelper.shouldSendWomenGif
+
+@OptIn(RiskFeature::class)
+fun BehaviourContext.observeAudioMessages(
+    scope: CoroutineScope
+) {
+    audioMessages().subscribe(scope = scope) { message ->
+        if (shouldSendLongLiveTheKingSticker(message)) {
+            when (message.chat) {
+                is GroupChatImpl -> sendLongLiveTheKingSticker(message)
+                is SupergroupChatImpl -> sendLongLiveTheKingSticker(message)
+                is ForumChatImpl -> sendLongLiveTheKingSticker(message)
+                else -> Unit
+            }
+            return@subscribe
+        }
+
+        if (shouldSendWomenGif(message)) {
+            when (message.chat) {
+                is GroupChatImpl -> sendWomenGif(message)
+                is SupergroupChatImpl -> sendWomenGif(message)
+                is ForumChatImpl -> sendWomenGif(message)
+                else -> Unit
+            }
+            return@subscribe
+        }
+    }
+}
